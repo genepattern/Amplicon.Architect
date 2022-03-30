cf46558f51097e5c8bf59c858bcfeec9a62cd46f
ted Liefeld <liefeld@broadinstitute.org>
2022-03-30 13:53:39 -0700

    adding example of how to run gpunit

---
+++ gpunit/runtest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export GPUNIT_HOME=/Users/liefeld/GenePattern/gp_dev/GpUnit
+
+# fill in username and password
+ant -f ${GPUNIT_HOME}/build.xml -Dgpunit.diffStripTrailingCR="--strip-trailing-cr" -Dgp.host="gp-beta-ami.genepattern.org" -Dgp.url="https://beta.genepattern.org" -Dgp.user="" -Dgp.password="" -Dgpunit.testfolder=`pwd` gpunit
