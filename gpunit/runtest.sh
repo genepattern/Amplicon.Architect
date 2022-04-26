8e615b839ec7c265250321664dc3d0717428dda9
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2022-04-26 16:00:35 -0700

    - Fixed GP Unit Test File

--- gpunit/runtest.sh
+++ gpunit/runtest.sh
@@ -3,4 +3,4 @@
 export GPUNIT_HOME=/Users/liefeld/GenePattern/gp_dev/GpUnit
 
 # fill in username and password
-ant -f ${GPUNIT_HOME}/build.xml -Dgpunit.diffStripTrailingCR="--strip-trailing-cr" -Dgp.host="gp-beta-ami.genepattern.org" -Dgp.url="https://beta.genepattern.org" -Dgp.user="" -Dgp.password="" -Dgpunit.testfolder=`pwd` gpunit
+ant -f $GPUNIT_HOME/build.xml -Dgpunit.diffStripTrailingCR="--strip-trailing-cr" -Dgp.host="gp-beta-ami.genepattern.org" -Dgp.url="https://beta.genepattern.org" -Dgp.user="edwin5588" -Dgp.password="edwin123" -Dgpunit.testfolder=`pwd` gpunit
