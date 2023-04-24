4b45e3f7a54abf9568276b99808fa0ccc99a2205
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2023-04-24 11:19:37 -0700

    Delete runtest.sh

--- gpunit/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#  get GPUNIT from https://github.com/genepattern/GPUnit (private repo for GP team)
-export GPUNIT_HOME=/Users/liefeld/GenePattern/gp_dev/GpUnit
-
-# fill in username and password
-ant -f $GPUNIT_HOME/build.xml -Dgpunit.diffStripTrailingCR="--strip-trailing-cr" -Dgp.host="gp-beta-ami.genepattern.org" -Dgp.url="https://beta.genepattern.org" -Dgp.user="edwin5588" -Dgp.password="edwin123" -Dgpunit.testfolder=`pwd` gpunit
